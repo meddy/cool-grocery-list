a30037abc4745ee0bd8f060e50c69a19db07620c
Max Eddy <maxiscool@gmail.com>
2020-09-05 14:32:02 -0700

    Add firebaseui for auth.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,36 @@
+import * as firebase from "firebase/app";
+import * as firebaseui from "firebaseui";
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "firebase/auth";
+import "firebaseui/dist/firebaseui.css";
+
 import App from "./App";
+import firebaseConfig from "./firebase.config.json";
 import * as serviceWorker from "./serviceWorker";
+
+firebase.initializeApp(firebaseConfig);
+
+// firebase.auth().signOut();
+
+const authUI = new firebaseui.auth.AuthUI(firebase.auth());
+firebase.auth().onAuthStateChanged((user) => {
+  if (!user) {
+    authUI.start("#firebaseui-auth-container", {
+      callbacks: {
+        signInSuccessWithAuthResult: function () {
+          return false;
+        },
+      },
+      signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      ],
+    });
+  }
+  console.log(user);
+});
 
 ReactDOM.render(
   <React.StrictMode>
